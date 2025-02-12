758b52baaae4cfafec7b0e2d6b0bb3c6bc0ba995
kimgab <nasayaokim@gmail.com>
2025-02-12 16:50:18 +0800

    feat: Add StudentTable.py to MainWindow using dynamic ui generation

--- src/views/MainWindow.py
+++ src/views/MainWindow.py
@@ -1,7 +1,8 @@
-import sys
 from PyQt6 import uic
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QSizePolicy
 from PyQt6.QtGui import QIcon
+
+from views.StudentTable import StudentTable
 
 class MainWindow(QMainWindow):
   def __init__(self):
@@ -15,7 +16,11 @@ class MainWindow(QMainWindow):
     self.setWindowTitle("Lexis")
 
     # UI COMPONENTS
-    
+    self.studentTable = StudentTable(self)
+    self.dataFrame.layout().addWidget(self.studentTable)
+
+    # Ensure StudentTable expands freely inside scroll area
+    self.studentTable.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
   # ---------------------------------------------------------
   
